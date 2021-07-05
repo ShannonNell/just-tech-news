84f19280fad8bedefc28a221d7fd6e4d1e3889b6
Shannon <shannon.nell92@gmail.com>
2021-07-05 14:11:27 -0400

    Create api routes for post model

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
+//force: true - means db will make tables re-create if any association changes
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
